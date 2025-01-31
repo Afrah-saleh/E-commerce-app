e2d5b9310bc681fec23a5a8c1e51c09d1b912d64
Afrah-saleh <afrahsaleh49@hotmail.com>
2025-01-31 23:53:34 +0300

    Initial Commit

---
+++ E-commerce app/E_commerce_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_commerce_appApp.swift
+//  E-commerce app
+//
+//  Created by Afrah Saleh on 01/08/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct E_commerce_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
